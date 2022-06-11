d6249b3e26d3ac49c9f961d1be06f33469d05b68
José Miguel Álvarez Vañó <jm.alvarez.vano@gmail.com>
2022-06-11 09:53:44 +0200

    Remove GitHub top banner

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,14 @@
-import Alert from '../components/alert'
-import Footer from '../components/footer'
-import Meta from '../components/meta'
+import Footer from "../components/footer";
+import Meta from "../components/meta";
 
 export default function Layout({ preview, children }) {
   return (
     <>
       <Meta />
       <div className="min-h-screen">
-        <Alert preview={preview} />
         <main>{children}</main>
       </div>
       <Footer />
     </>
-  )
+  );
 }
