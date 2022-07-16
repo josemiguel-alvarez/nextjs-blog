f14562266cb6079b944a6560486421cb4c2be3f9
José Miguel Álvarez <jose.alvarez@shopify.com>
2022-07-16 18:24:55 +0200

    Fix build errors

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -16,16 +16,16 @@ const siteMetadata = {
   twitter: "https://twitter.com/jmalvarez_dev",
   linkedin: "https://linkedin.com/in/jmalvarezvano",
   locale: "en-US",
-  // analytics: {
-  //   // If you want to use an analytics provider you have to add it to the
-  //   // content security policy in the `next.config.js` file.
-  //   // supports plausible, simpleAnalytics, umami or googleAnalytics
-  //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-  //   simpleAnalytics: false, // true or false
-  //   umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-  //   googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
-  //   posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
-  // },
+  analytics: {
+    //   // If you want to use an analytics provider you have to add it to the
+    //   // content security policy in the `next.config.js` file.
+    //   // supports plausible, simpleAnalytics, umami or googleAnalytics
+    plausibleDataDomain: "", // e.g. tailwind-nextjs-starter-blog.vercel.app
+    simpleAnalytics: false, // true or false
+    umamiWebsiteId: "", // e.g. 123e4567-e89b-12d3-a456-426614174000
+    googleAnalyticsId: "", // e.g. UA-000000-2 or G-XXXXXXX
+    posthogAnalyticsId: "", // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
+  },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  experimental: {
-    optimizeUniversalDefaults: true,
-  },
   content: [
     "./pages/**/*.js",
     "./components/**/*.js",
