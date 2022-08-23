03c7745199068797b7fec47a7f6f6e8fac70ad4b
José Miguel Álvarez Vañó <jm.alvarez.vano@gmail.com>
2022-08-23 12:55:36 +0200

    Add banner if not supported

--- components/posts/using-has-pseudo-class-as-parent-selector/example.js
+++ components/posts/using-has-pseudo-class-as-parent-selector/example.js
@@ -1,5 +1,9 @@
 export const HasPseudoClassExample = () => (
   <div className="HasPseudoClassExample">
+    <small className="has-not-supported">
+      This demo requires a browser that supports <code>:has()</code>.
+    </small>
+
     <div>
       <img src="/static/images/ocean.jpeg" />
       <p>Image description.</p>
